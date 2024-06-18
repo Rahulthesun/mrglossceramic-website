9b9cccdb667648f071d1f660c6e8c44a6fbfe545
unknown <rahulthesun82@gmail.com>
2024-06-18 13:20:43 +0530

    Links Update and few structal updates

--- mrglossceramic/base/views.py
+++ mrglossceramic/base/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
     return render(request ,'base/home.html')
+
+def franchise_page(request):
+    return render(request, "base/franchise.html")
